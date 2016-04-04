4ab59bfb17cffac74467b85717aca3cf089f91c3
kodiful <uchiyama@mac.com>
2016-04-04 23:06:59 +0900

    ロゴを変更

--- resources/lib/radiru.py
+++ resources/lib/radiru.py
@@ -20,10 +20,6 @@ __settings_file__ = os.path.join(__radiru_path__, 'settings.xml')
 __player_url__    = 'http://www3.nhk.or.jp/netradio/files/swf/rtmpe201406.swf?ver.2'
 __station_url__   = 'http://www3.nhk.or.jp/netradio/app/config_pc.xml'
 __program_url__   = 'http://www2.nhk.or.jp/hensei/api/noa.cgi'
-__biglogo_url__   = 'http://www3.nhk.or.jp/netradio/files/img/biglogo.gif'
-#__biglogo_url__  = 'http://www.nhk.or.jp/radiru/common/images/footer_logo01.gif'
-#__biglogo_url__  = 'http://www.nhk.or.jp/radiru/common/images/footer_logo02.gif'
-#__biglogo_url__  = 'http://www.nhk.or.jp/radiru/common/images/footer_logo03.gif'
 
 #-------------------------------------------------------------------------------
 class Radiru:
@@ -35,9 +31,7 @@ class Radiru:
             self.area = ['東京','仙台','名古屋','大阪'][int(area)]
         except:
             self.area = '東京'
-        self.protocol = 'rtmp'
-        #self.protocol = 'wms'
-    
+
     def getStationFile(self):
         # キャッシュがある場合
         if os.path.isfile(__station_file__):
@@ -63,7 +57,7 @@ class Radiru:
         dom = xml.dom.minidom.parseString(xmlstr)
         results = []
         settings = []
-        stations = [['NHKR1','NHKラジオ第1','r1','r1_wms'],['NHKR2','NHKラジオ第2','r2','r2_wms'],['NHKFM','NHK FM','fm','fm_wms']]
+        stations = [['NHKR1','NHKラジオ第1','r1','http://www.nhk.or.jp/r2/images/symbol_r1.png'],['NHKR2','NHKラジオ第2','r2','http://www.nhk.or.jp/r2/images/symbol_r2.png'],['NHKFM','NHK FM','fm','http://www.nhk.or.jp/r2/images/symbol_fm.png']]
         i = -1
         data = dom.getElementsByTagName('data')
         for datum in data:
@@ -73,20 +67,14 @@ class Radiru:
                     # pack as xml
                     id = station[0].decode('utf-8')
                     name = station[1].decode('utf-8')
-                    if self.protocol == 'rtmp':
-                        url = datum.getElementsByTagName(station[2])[0].firstChild.data.strip()
-                        url = '%s swfUrl=%s swfVfy=1 live=1' % (url,__player_url__)
-                    elif self.protocol == 'wms':
-                        url = datum.getElementsByTagName(station[3])[0].firstChild.data.strip()
-                        response = urllib.urlopen(url)
-                        data = response.read()
-                        response.close()
-                        url = xml.dom.minidom.parseString(data).getElementsByTagName('REF')[0].getAttribute('HREF')
+                    url = datum.getElementsByTagName(station[2])[0].firstChild.data.strip()
+                    url = '%s swfUrl=%s swfVfy=1 live=1' % (url,__player_url__)
+                    logo = station[3].decode('utf-8')
                     # pack as xml
                     xmlstr = '<station>'
                     xmlstr += '<id>radiru_%s</id>' % (id)
                     xmlstr += '<name>%s</name>' % (name)
-                    xmlstr += '<logo_large>%s</logo_large>' % (__biglogo_url__)
+                    xmlstr += '<logo_large>%s</logo_large>' % (logo)
                     xmlstr += '<url>%s</url>' % (url)
                     xmlstr += '</station>'
                     results.append(xmlstr)
@@ -104,13 +92,13 @@ class Radiru:
         f.write('\n'.join(settings))
         f.close()
         return '\n'.join(results)
-    
+
     def getSettingsArray(self):
         f = codecs.open(__settings_file__,'r','utf-8')
         settings = f.read()
         f.close()
         return settings
-    
+
     def getProgramFile(self):
         response = urllib.urlopen(__program_url__)
         data = response.read()
